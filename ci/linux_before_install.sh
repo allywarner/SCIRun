d6c9e300ad22813b34fc9c7da36691ccd446c596
ayla <ayla@sci.utah.edu>
2015-03-18 23:20:41 -0600

    Try configuring linux build to use clang 3.5 and libstdc++ 4.9 to fix boost compile problem.

--- ci/linux_before_install.sh
+++ ci/linux_before_install.sh
@@ -2,4 +2,8 @@
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
+# next 2 lines necessary until Ubuntu 14
+sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main' -y
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key-add -
+# done
 sudo apt-get update -qq
--- ci/linux_install.sh
+++ ci/linux_install.sh
@@ -3,6 +3,15 @@
 sudo apt-get install -qq cmake
 sudo apt-get install -qq libosmesa6-dev
 sudo apt-get install -qq libqt4-dev qt4-qmake
-echo "$CXX"
-if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
-if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
+
+if [ "$CXX" = "g++" ]; then
+  sudo apt-get install -qq g++-4.8
+  export CXX="g++-4.8" CC="gcc-4.8"
+  echo "$CXX"
+fi
+
+if [ "$CXX" = "clang++" ]; then
+  sudo apt-get install llvm-3.5 llvm-3.5-dev clang-3.5 libstdc++-4.9-dev
+  export CXX="clang++-3.5" CC="clang-3.5"
+  $CXX --version
+fi
