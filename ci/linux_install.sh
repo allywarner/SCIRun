28b4f93d2a8274f81182a78581abcaa75ec2a7c7
ayla <ayla@sci.utah.edu>
2015-03-19 13:25:02 -0600

    Try removing old compiler to ensure new clang is used.

--- ci/linux_install.sh
+++ ci/linux_install.sh
@@ -7,11 +7,12 @@ sudo apt-get install -qq libqt4-dev qt4-qmake
 if [ "$CXX" = "g++" ]; then
   sudo apt-get install -qq g++-4.8
   export CXX="g++-4.8" CC="gcc-4.8"
-  echo "$CXX"
+  $CXX --version
 fi
 
 if [ "$CXX" = "clang++" ]; then
   sudo apt-get install -qq llvm-3.5 llvm-3.5-dev clang-3.5 libstdc++-4.9-dev
+  sudo apt-get remove llvm-3.4 clang-3.4
   export CXX="clang++-3.5" CC="clang-3.5"
   $CXX --version
 fi
