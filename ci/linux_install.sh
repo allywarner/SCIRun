325786dd6408df7c2bc3edec1b82b5014e2f1ad1
Dan White <dwhite@sci.utah.edu>
2015-05-05 15:49:09 -0600

    Update linux_install.sh

--- ci/linux_install.sh
+++ ci/linux_install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install -qq cmake
 sudo apt-get install -qq libosmesa6-dev
-sudo apt-get install -qq libqt5-dev qt5-qmake
+sudo apt-get install -qq libqt5-dev qt5-qmake qt5-default qttools5-dev-tools
 echo "$CXX"
 if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
 if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
